dbf7223949555d0ba7f73188bdfbab227c84b3c1
Matthis Krause <matthiskrause@hotmail.com>
2014-05-04 10:10:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Überfachliche Kompetenzen 1b
+Meine Anteil der Semesterarbeit über die Abhandlung zum Thema '"Internet und digitale Gesellschaft" -- Interoperabilität, Standards, Freie Software' der Enquete-Kommission des deutschen Bundestages.
---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+bibfile=bibliography.bib
+templ=latex.template
+cslfile=harvard7de.csl
+
+if [ "$2" == "latex" ]
+then
+	pandoc -V lang=ngerman -f markdown -t latex --template $templ --biblatex \
+		--toc --csl=$cslfile --number-sections --biblio=$bibfile $1.md > $1.tex && \
+	pdflatex $1.tex
+
+elif [ "$1" == "clean" ]
+then
+	rm *.bbl *.blg *.out *.run.xml *.toc *.log *.bcf *.aux -v
+
+else
+	pandoc -V lang=ngerman -o $1.pdf --biblio=$bibfile $1.md
+
+fi
